4aa53d7cf9205d66caf9f1adc41a89a91af24ef1
Marko Heijnen <info@markoheijnen.com>
2013-09-14 20:09:39 +0200

    Start iOS project

---
+++ WordCamp/WordCamp/WordCampAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  WordCampAppDelegate.h
+//  WordCamp
+//
+//  Created by Marko Heijnen on 14-09-13.
+//  Copyright (c) 2013 CodeKitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WordCampAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
